57fefa5f75062ee7cf8a7e1ecb6e31864c8b3c44
ronneyTorres <ronneypris26@gmail.com>
2023-12-17 10:27:29 -0300

    Foi adicionado estrutura de repetição para incremento automático de vitórias.

--- desafioClassificacao.js
+++ desafioClassificacao.js
@@ -1,26 +1,28 @@
-let nivelHeroi = '';
+let qtdVitorias = 0;
+let qtdDerrotas = 0;
 
 function classificaHeroiPorVitoria(qtdVitorias, qtdDerrotas) {
   saldoVitorias = qtdVitorias - qtdDerrotas;
-  if (saldoVitorias <= 10) {
-    nivelHeroi = 'Ferro';
-  } else if (saldoVitorias <= 20) {
-    nivelHeroi = 'Bronze';
-  } else if (saldoVitorias <= 50) {
-    nivelHeroi = 'Prata';
-  } else if (saldoVitorias <= 80) {
-    nivelHeroi = 'Ouro';
-  } else if (saldoVitorias <= 90) {
-    nivelHeroi = 'Diamante';
-  } else if (saldoVitorias <= 100) {
-    nivelHeroi = 'Lendário';
-  } else {
-    nivelHeroi = 'Imortal';
+  //Adicionado estrutura de repetição apenas para incremento automático de vitórias.
+  //Caso seja apenas para verificação simples de nivel, so remover laço (For) e adicionar valores.
+  for(i = saldoVitorias; i <= 130; i = i + 10) {
+    if (i <= 10) {
+      nivelHeroi = 'Ferro';
+    } else if (i <= 20) {
+      nivelHeroi = 'Bronze';
+    } else if (i <= 50) {
+      nivelHeroi = 'Prata';
+    } else if (i <= 80) {
+      nivelHeroi = 'Ouro';
+    } else if (i <= 90) {
+      nivelHeroi = 'Diamante';
+    } else if (i <= 100) {
+      nivelHeroi = 'Lendário';
+    } else {
+      nivelHeroi = 'Imortal';
+    }
+    console.log(`O Herói tem um saldo de ${i} vitórias e está no nivel ${nivelHeroi}`)
   }
-  return qtdVitorias
 }
 
-classificaHeroiPorVitoria(20,0);
-//console.log(`O Herói tem um saldo de ${saldoVitorias} vitórias e está no nivel ${nivelHeroi}`)
-
-console.log(qtdVitorias);
+classificaHeroiPorVitoria(qtdVitorias,qtdDerrotas);
